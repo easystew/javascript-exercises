b436e68afaa12006838af52f049f0e4da7abdc57
Ezekiel Stewart <ezekielsimonstewart@icloud.com>
2025-09-01 17:14:34 -0400

    Completed exercises.

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,10 +1,11 @@
-const repeatString = function (word, times) {
-    if (times < 0) return "ERROR";
-    let string = "";
-    for (let i = 0; i < times; i++) {
-        string += word;
+const repeatString = function (string, num) {
+    if (num < 0) {
+        return "ERROR";
     }
-    return string;
-};
-
+    let newString = "";
+    for (let i = 0; i < num; i++) {
+        newString += string;
+    }
+    return newString;
+}
 module.exports = repeatString;
--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,4 +1,21 @@
-const sumAll = function() {
+const sumAll = function (int1, int2) {
+
+    sum = 0
+    if (!Number.isInteger(int1) || int1 < 0 || !Number.isInteger(int2) || int2 < 0) {
+        return "ERROR";
+    }
+
+    if (int1 > int2) {
+        for (let i = int1; i >= int2; i--) {
+            sum += i;
+        }
+    }
+    else if (int1 < int2) {
+        for (let i = int1; i <= int2; i++) {
+            sum += i;
+        }
+    }
+    return sum;
 
 };
 
