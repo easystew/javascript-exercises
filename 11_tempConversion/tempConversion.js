52b2984a8a22dd651329aebb2851c0d85a74cc47
Ezekiel Stewart <ezekielsimonstewart@icloud.com>
2025-09-01 17:42:21 -0400

    completed exercises

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (num) {
+  numBeforeRoundingc = ((num - 32) * (5 / 9));
+  return Math.round(numBeforeRoundingc * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (num) {
+  numBeforeRoundingf = (num * (9 / 5) + 32);
+  return Math.round(numBeforeRoundingf * 10) / 10;
 };
 
 // Do not edit below this line
